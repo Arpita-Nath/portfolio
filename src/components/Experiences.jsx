e7aee29c237712a1303ef95c92c2009b98d644ef
Arpita-Nath <arpitanath39@gmail.com>
2025-01-19 14:02:15 +0600

    training

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -10,7 +10,7 @@ const Experiences = () => {
         whileInView={{opacity: 1, y: 0}}
         initial={{opacity: 0, y: -100}}
         transition={{duration: 1.5}}
-        className='my-20 text-center text-4xl text-[#00e1ff]'>Experiences</motion.h2>
+        className='my-20 text-center text-4xl text-[#00e1ff]'>Training and Experience</motion.h2>
         <div>
             {EXPERIENCES.map((item,index) => (
             <div key={index} className='mb-8 flex flex-wrap lg:justify-center'>
